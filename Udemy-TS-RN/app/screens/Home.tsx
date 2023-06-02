8cc38b2d781a646be6aeff0723973588cf4153e6
patxi <patxiarana05@gmail.com>
2023-06-02 18:36:45 -0300

    styled icon

--- Udemy-TS-RN/app/screens/Home.tsx
+++ Udemy-TS-RN/app/screens/Home.tsx
@@ -1,12 +1,13 @@
 import { FC } from 'react';
 import {StyleSheet, TouchableOpacity, View,Text} from 'react-native';
-
+import { Entypo, AntDesign  } from '@expo/vector-icons';  
 interface Props {}
 const Home: FC<Props> = (): JSX.Element => {
   return <View style={styles.container}>
     {/* Image Capture  Button  */ }
     <View  style={styles.btnContainer}>
   <TouchableOpacity style={styles.button}>
+  <Entypo name="camera" style={styles.icon} />
   </TouchableOpacity>
   <Text style={styles.btnLabel}>Capture</Text>
   </View>
@@ -14,6 +15,7 @@ const Home: FC<Props> = (): JSX.Element => {
       {/* Image Select Button  */ }
   <View  style={styles.btnContainer}>
   <TouchableOpacity style={styles.button}>
+  <AntDesign name="folderopen" style={styles.icon} />
   </TouchableOpacity>
   <Text style={styles.btnLabel}>Select</Text>
   </View >
@@ -25,6 +27,7 @@ const styles = StyleSheet.create({
   //  flex: 1 ,
    justifyContent : 'center', 
    alignItems : 'center' , 
+   marginTop: 200,
   // display: 'none', 
   },
   btnContainer : {
@@ -38,10 +41,16 @@ button: {
      borderWidth : 4 ,
     borderColor : '#6C9ADE' ,
     borderRadius : 7 , 
+    alignItems : 'center',
+    justifyContent: 'center'
 },
 btnLabel:{
     textAlign : 'center' , 
     fontWeight : '500' , 
+},
+icon : {
+    fontSize:55,
+    color:"#6C9ADE",
 }
 });
 
